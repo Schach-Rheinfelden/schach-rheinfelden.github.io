13aad612fbd22cd9a390296c51990e35da402506
Schach-Rheinfelden <schach@sfpelikan.org>
2026-08-08 22:17:10 +0200

    Auto-Update via Tool

--- js/events-archive.js
+++ js/events-archive.js
@@ -385,8 +385,8 @@ window.openEventModal = function(id) {
 
     modalBody.innerHTML = `
         ${headerImgHTML}
-        <div style="display: flex; justify-content: space-between; align-items: flex-start;">
-            <div>
+        <div class="modal-head">
+            <div class="modal-head-text">
                 <div style="font-size: 0.9rem; color: ${accentCol}; margin-bottom: 0.5rem; text-transform: uppercase; letter-spacing: 1px; font-weight: 700;">
                     ${metaLine}
                 </div>
